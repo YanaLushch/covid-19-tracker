118ebc113ebbbed2489962b62fbf9687183da0b3
Jana Lushch <yana.lushch@gmail.com>
2021-01-12 13:53:43 +0200

    Created the covid-19 tracker globally and by the country

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './App.module.css';
+
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import { fetchData } from './api/index';
+
+import coronaImage from './images/image.png';
+
+class  App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+
+   async componentDidMount(){
+     const fetchedData = await fetchData();
+     
+     this.setState({data: fetchedData})
+   }
+   
+   handleCountryChange = async (country) => {
+     //fetch data
+    const fetchedData = await fetchData(country);
+    this.setState({data: fetchedData, country: country})
+    
+    //set the state
+   }
+
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+
+      <div className={styles.container}>
+        <img className={styles.png} src={coronaImage} alt="COVID-19"></img>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+        <Chart data={data} country={country} />
+        
+      </div>
+    );
+  }
+  
+}
+
+export default App;
